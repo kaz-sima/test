171c1fc1a68029fdc29e52fdf5dcc9166fb8e09a
kaz-sima <simanaka@sage.ocn.ne.jp>
2020-02-24 11:40:30 +0630

    Login

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,17 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    static $password;
+    
+    static $seed = 0;
+    $faker->seed($seed++);   
+    
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        //'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('user'), // guserh is the password
         'remember_token' => Str::random(10),
     ];
 });
